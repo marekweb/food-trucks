6de08c17383e37b0783ce7141839a9174f7f7080
Marek <marekz@gmail.com>
2017-12-07 14:32:14 -0500

    Simplify event.js

--- src/event.js
+++ src/event.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
 export default function Event(props) {
-  const { name, date, address, attending } = props;
-
   return <div>
-    <h3>{name}</h3>
+    <h3>{props.name}</h3>
     <ul>
       <li>
-        <b>Date:</b> {date}
+        <b>Date:</b> {props.date}
       </li>
       <li>
-        <b>Address:</b> {address}
+        <b>Address:</b> {props.address}
       </li>
       <li>
         <b>Start time: {/* TODO: relative time here */}</b>
       </li>
       <li>
-        <b>Trucks Attending: {attending.length}</b>
+        <b>Trucks Attending: {props.attending.length}</b>
         <ul>
           {/* TODO: attendees here */}
         </ul>
